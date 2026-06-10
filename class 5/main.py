e3617efdc6754827d4bc7c3893c280542bc83efb
KashanAdnan02 <fullstackdevkashan@gmail.com>
2026-06-10 17:11:50 +0500

    class 5 if statement elif and operator input

---
+++ class 5/main.py
@@ -0,0 +1,67 @@
+# print("class 5")
+
+
+# sytax  ==> likhne ka tareka
+
+# if condition:
+#     print("Hello")
+
+
+# marks = 100
+
+# if marks < 90: # True
+#     print("A+ grade")
+# else:
+#     print("Fail")
+
+
+
+# and
+# or
+
+  ## TRUE       # TRUE
+# condition and conditon 
+
+# if marks < 40:
+#     print("Fail")
+# elif marks > 40 and marks < 60:
+#     print("pass")
+# elif marks > 60 and marks < 70:
+#     print("C grade")
+# elif marks > 70 and marks < 80:
+#     print("B grade")
+# elif marks > 80 and marks < 90:
+#     print("A grade")
+# elif marks > 90 and marks < 100:
+#     print("A+ grade")
+    
+# elif marks == 100:
+#     print("You are the topper")
+
+
+# marks = 73
+
+
+# if marks > "70":
+#     print("Hello")
+
+
+
+# elif
+
+# and
+
+
+num1 = input("Enter any random number :  ")
+num2 = input("Enter any 2nd random number : ")
+
+newNum1 = int(num1)
+newNum2 = int(num2)
+print(newNum1 + newNum2)
+# print(type(num1))
+# print(type(num2))
+# print(num1 + num2)
+# +
+
+# "12" + "90" = 1290
+# 12 + 90 = 102
