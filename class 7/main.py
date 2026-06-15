7ec3779eb863e2b1ee9f2f9cbfc1bc0c63cdf265
KashanAdnan02 <fullstackdevkashan@gmail.com>
2026-06-15 17:20:57 +0500

    done

---
+++ class 7/main.py
@@ -0,0 +1,77 @@
+print("Hello it is working!")
+
+score = 0
+
+num = 90
+
+# if  40 > num:  # False
+#     print("it's true")
+# elif num < 50:
+#     print("50 is less than num")
+# else:
+#     print("it's false")
+#     score = score + 1
+#     print(score)
+
+
+# if num < 8:
+#     print()
+    
+# if num > 90:
+
+# print("Hello World")
+# print("Hello World")
+# print()
+
+# Loop 
+# for keyword
+# for num in range(1, 11):
+#     print(num * 2)
+    
+# 1
+# 2
+# 3
+
+
+
+# print("2 x 1 = 2")
+# print("2 x 2 = 4")
+# print("2 x 3 = 6")
+
+
+
+
+# print("hello")
+
+
+
+# for
+
+# for num in range(1, 8):
+#     # code
+
+
+# for num in range(2, 9):
+#     print("Hello")
+
+
+
+# Table Generator
+
+# 2 X 1 = 2
+# 2 X 2 = 4
+
+table = 38769
+
+for num in range(1 , 11):
+    print(table * num)
+    # 2 * 1
+    # 2 * 2
+    # 2 * 3
+    # 2 * 4
+    # 2 * 5
+    # 2 * 6
+    # 2 * 7
+    # 2 * 8
+    # 2 * 9
+    # 2 * 10
