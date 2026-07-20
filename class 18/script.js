3738e8d333069f9720eb0679a4c5c824ffa61dc8
KashanAdnan02 <fullstackdevkashan@gmail.com>
2026-07-20 17:19:52 +0500

    done

---
+++ class 18/script.js
@@ -0,0 +1,30 @@
+// console.log(document);
+
+// let a = document.getElementsByTagName("p");
+// console.log(a);
+
+//
+// function handleSum() {
+//   let b = 2;
+//   let c = 2;
+
+//   let d = b + c;
+//   a[2].innerHTML = d;
+//   a[2].style.marginLeft = "50px";
+// }
+
+
+function move() {
+    let div = document.getElementsByTagName("div")
+    // console.log()
+    div[0].style.marginLeft = "200px"
+    div[0].style.borderRadius = "50%"
+    div[0].style.rotate = "2000deg"
+    div[0].style.background = "green"
+
+    div[0].style.textDecoration = "underline"
+    div[0].style.fontStyle = "italic"
+    div[0].style.fontWeight = "bold"
+
+
+}
---
+++ class 19/script.js
@@ -0,0 +1,21 @@
+console.log('javascript connected')
+
+
+// DOM
+// Document Object Model
+
+// console.log(document)
+
+
+//
+
+// let name = "Kashan"
+
+
+let h1 = document.getElementsByTagName("h1")
+// document.getElementById
+let ol = document.getElementById("list1")
+// document.getElementsByClassName
+
+console.log(ol)
+console.log(h1)
